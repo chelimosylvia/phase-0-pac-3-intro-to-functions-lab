e81cbe9e9a6a94537c5e1e2b745bc7120b655cb3
Sylviachelimo <chelimosylvia280@gmail.com>
2024-01-11 12:39:09 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout (hello) {
+    console.log(hello.toUpperCase( ));
+}
+function logWhisper (hello) {
+    console.log(hello.toLowerCase( ));
+}
+function sayHiToHeadphonedRoommate (hello) {
+var cantAnswer = "I can't hear you!";
+var yesAnswer =  "YES INDEED!";
+var lovAnswer = "I would love to!"
+if (hello.toLowerCase(hello) === hello){
+    return cantAnswer;
+
+}
+else if (hello.toUpperCase(hello) === hello){
+    return yesAnswer;
+
+}
+else if ("Let's have dinner together!" === hello){
+    return lovAnswer
+}
+}
